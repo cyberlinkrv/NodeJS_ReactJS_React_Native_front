d14339a4260e0a1b773f232d1b65022236944b95
Divino J. Silva <caiaponianet@hotmail.com>
2020-08-06 16:47:20 -0300

    Criando componente raiz

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-const soma = (a, b) => a +b;
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
 
-alert(soma(1, 3));
+render(< App />, document.getElementById('app'));
